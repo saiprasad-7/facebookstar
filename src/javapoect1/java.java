51e2b38e99a402c25f1ee9b16949bb6238fef938
sai <sai@192.168.0.11>
2020-05-30 16:46:54 +0530

    second change

--- src/javapoect1/java.java
+++ src/javapoect1/java.java
@@ -1,5 +1,22 @@
 package javapoect1;
 
-public class java {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class java 
+{
+@Test
+public void logn(){
+
+WebDriver driver=new ChromeDriver();
+driver.get("https://facebookstar.com");
+driver.close();
+driver.quit();
+
 
 }
+		
+}
+
+
